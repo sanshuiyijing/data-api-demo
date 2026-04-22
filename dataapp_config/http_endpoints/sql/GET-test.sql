133d7c4f7afc81076c7d4df231e10394c0295b6b
sanshuiyijing <jingdeveloper@126.com>
2026-04-22 17:40:31 +0800

    feat: test p0

--- dataapp_config/http_endpoints/sql/GET-test.sql
+++ dataapp_config/http_endpoints/sql/GET-test.sql
@@ -3,5 +3,5 @@ Enter "USE {database};" before entering your SQL statements.
 Type "--your question" + Enter to try out AI-generated SQL queries
 Declare a parameter like "Where id = ${arg}".
 */
-SELECT "Hello World";
-SELECT * from github_sample;
+DELETE FROM github_sample;
+SELECT 'deleted' AS status;
