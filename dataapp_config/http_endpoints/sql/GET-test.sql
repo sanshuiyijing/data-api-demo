7dae5e178cfb44a3aa5fa7f55aebc4377ebfefec
sanshuiyijing <jingdeveloper@126.com>
2026-04-25 11:51:49 +0800

    fix: update text

--- dataapp_config/http_endpoints/sql/GET-test.sql
+++ dataapp_config/http_endpoints/sql/GET-test.sql
@@ -3,7 +3,7 @@ Enter "USE {database};" before entering your SQL statements.
 Type "--your question" + Enter to try out AI-generated SQL queries
 Declare a parameter like "Where id = ${arg}".
 */
-SELECT "Hello World";
+SELECT "Hello World!";
 SELECT * from github_sample;
 
 
